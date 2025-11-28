45be9b312baf54ac65a9a0c51f8cb6ff4a06be0a
Daniel Squair <dansquair2@gmail.comgit config --global user.name Daniel>
2025-11-28 18:39:14 -0500

    created api code structure and tested showing .json when clicking 'My Trips'

--- src/components/MyTrips.jsx
+++ src/components/MyTrips.jsx
@@ -1,7 +1,25 @@
+import {useEffect, useState } from 'react'
+
 export default function MyTrips() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/test")
+      .then(res => res.json())
+      .then(json =>setData(json))
+      .catch(err => console.error(err))
+  }, [])
+
+
+  const title = data?.title || ""
+  const desc = data?.desc || ""
+
   return (
     <div style={{ padding: "20px", color: "white" }}>
       <h1>My Trips</h1>
+      <h2>Test Trip</h2>
+      <p><strong>Title: </strong> {title}</p>
+      <p><strong>Description: </strong> {desc}</p>
     </div>
   )
 }
