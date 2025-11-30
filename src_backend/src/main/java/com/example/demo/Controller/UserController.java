48503e5e7892f1dba97c05d20fa64e21e914b2e6
Daniel Squair <dansquair2@gmail.comgit config --global user.name Daniel>
2025-11-30 17:18:25 -0500

    created api end points to access a mongodb, need to figure out how to connect mongodb without localhost:27017

---
+++ src_backend/src/main/java/com/example/demo/Controller/UserController.java
@@ -0,0 +1,41 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Model.User;
+import com.example.demo.Service.UserService;
+import com.example.demo.UserDTO.UserTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createUser(@RequestBody UserTO user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/get")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getUser() {
+        return userService.getUsers();
+    }
+
+    @DeleteMapping("/delete")
+    @ResponseStatus(HttpStatus.OK)
+    public String deleteUser(@RequestParam String id) {
+        return userService.deleteUser(id);
+    }
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.OK)
+    public String updateUser(@RequestBody UserTO user) {
+        return userService.updateUser(user);
+    }
+}
---
+++ src_backend/src/main/java/com/example/demo/Service/UserService.java
@@ -0,0 +1,66 @@
+package com.example.demo.Service;
+
+import com.example.demo.Model.User;
+import com.example.demo.Repository.UserRepository;
+import com.example.demo.UserDTO.UserTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepo;
+
+    public String createUser(UserTO userTO) {
+        try {
+            User user = User.builder()
+                    .email(userTO.getEmail())
+                    .password(userTO.getPassword())
+                    .build();
+            userRepo.save(user);
+
+        } catch (Exception e){
+            return "Error creating user";
+        }
+
+        return "User Created Successfully";
+    }
+
+    public List<User> getUsers() {
+        List<User> userList = new ArrayList<>();
+        try {
+            userList = userRepo.findAll();
+        } catch (Exception e) {
+        }
+
+        return userList;
+    }
+
+    public String deleteUser(String id) {
+        try {
+            userRepo.deleteById(id);
+        } catch (Exception e) {
+            return "Error Deleting User";
+        }
+
+        return "User Deleted Successfully";
+    }
+
+    public String updateUser(UserTO userTO) {
+        try {
+            User user = User.builder()
+                    .id(userTO.getId())
+                    .email(userTO.getEmail())
+                    .password(userTO.getPassword())
+                    .build();
+            userRepo.save(user);
+        } catch (Exception e) {
+            return "Error Updating User";
+        }
+
+        return "User Updated Successfully";
+    }
+}
